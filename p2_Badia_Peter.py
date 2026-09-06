a345530ff81abac7a1f49881cb52fba26143b074
Tonyb712 <78864470+Tonyb712@users.noreply.github.com>
2026-09-06 18:55:50 -0400

    Update p2_Badia_Peter.py
    updated file with comments

--- p2_Badia_Peter.py
+++ p2_Badia_Peter.py
@@ -1,3 +1,5 @@
+#defining a function to iterate through numbers finding a match for a^2 + b^2
+#that equaLS C^2 then printing the triple
 def find_pythagorean(n) :
     c = n**2
     if n > 0 :
@@ -6,7 +8,7 @@ def find_pythagorean(n) :
                 if c == a**2 + b**2 :
                     return((a,b,n))
 
-
+#calling function
 n = int(input('Enter a positive number to find a triple: '))
 triples = find_pythagorean(n)
 print(triples)
