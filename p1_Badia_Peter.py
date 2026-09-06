571639753cfee07e7e4c804dcab40df37fa89370
Tonyb712 <78864470+Tonyb712@users.noreply.github.com>
2026-09-06 18:55:24 -0400

    Update p1_Badia_Peter.py
    updated file with comments

--- p1_Badia_Peter.py
+++ p1_Badia_Peter.py
@@ -1,6 +1,8 @@
+
 import math
 import matplotlib.pyplot as plt
 
+#while value has been added, continue with program
 while True :
     a = float(input('Enter a value for A'))
     
@@ -10,12 +12,18 @@ while True :
     b = float(input('Enter a value for B'))
     c = float(input('Enter a value for C'))
     
+    #performing the mathematical operation to find out whats under the square
+    #root
+    
     radicand = b**2-4*a*c
     
+    #the if statements for the possibility of certain answers
+    
+    #if  less than 0, close program and print message
     if radicand<0 :
         print('No real solutions')
         break 
-
+#if zero, print one solution and graph
     if radicand == 0 :
         x1 =-b+ math.sqrt(radicand)/(2*a)
         print('One solution:', x1)
@@ -23,7 +31,7 @@ while True :
         plt.axhline(0, color='black')
         plt.grid()
         plt.show()
-   
+   #if greater than 0, print two solutions with graph
     if radicand > 0 :
         x1 = -b+ math.sqrt(radicand)/(2*a)
         x2 = -b- math.sqrt(radicand)/(2*a)
